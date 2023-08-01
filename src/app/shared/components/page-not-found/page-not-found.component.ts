3892e40be50dd5200d259c18ba7886f1f6658702
muhammadali <muhammad.ali@lagorta.com>
2023-08-01 15:21:18 +0300

    Make page not found standalone component

--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,8 +1,11 @@
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'page-not-found',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularMaterialModule } from './angular-material.module';
-import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 import { SnackBarService } from '../services/snack-bar.service';
 
 const modules = [CommonModule, FormsModule, ReactiveFormsModule, AngularMaterialModule];
 
 @NgModule({
-    declarations: [PageNotFoundComponent],
+    declarations: [],
     imports: [
         ...modules
     ],
