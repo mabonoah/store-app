c5640bf235e39dae0d0756129a6642d0f922d706
muhammadali <muhammad.ali@lagorta.com>
2023-08-08 00:25:04 +0300

    Change spinner state and display http error handling in interceptor

--- src/app/core/services/http-interceptor.service.ts
+++ src/app/core/services/http-interceptor.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { SpinnerService } from 'src/app/shared/services/spinner.service';
+import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private spinner: SpinnerService,
+    private snackBar: SnackBarService
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get the auth token from the service.
@@ -16,9 +22,23 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', authToken)
     });
+    
+    this.spinner.show();
 
-    // send cloned request with header to the next handler.
-    return next.handle(authReq);
+    return next
+      .handle(authReq)
+      .pipe(
+        tap({
+          next: (event: HttpEvent<any>) => {
+            if (event instanceof HttpResponse)
+              this.spinner.hide();
+          },
+          error: (err: HttpErrorResponse) => {
+            this.spinner.hide();
+            this.snackBar.openFailureSnackBar(err.message, err.name);
+          }
+        })
+      );
   }
 }
 
